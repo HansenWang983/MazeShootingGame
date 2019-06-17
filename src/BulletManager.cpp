938bd7dd397b78ce445e089e11801e88d2e89a3a
hansenbeast <wangzehao1998@126.com>
2019-06-17 23:44:42 +0800

    add bullet

---
+++ src/BulletManager.cpp
@@ -0,0 +1,37 @@
+#include "BulletManager.h"
+
+BulletManager::BulletManager()
+{
+}
+
+BulletManager::~BulletManager()
+{
+	this->bulletLists.clear();
+}
+
+
+void BulletManager::newBullet(glm::vec3 front, glm::vec3 position) {
+	this->bulletLists.push_back(new Bullet(front, position));
+}
+
+void BulletManager::render(const glm::vec3 &lightPos, EulerCamera camera) {
+	for (int i = 0; i < (int)this->bulletLists.size(); i++) {
+        float distance = glm::distance(bulletLists[i]->getPosition(), camera.getPosition());
+	    if (distance > 100){
+		    deleteBullet(bulletLists[i]->id);
+        }else{
+		    bulletLists[i]->render(lightPos, camera);
+        }
+	}
+}
+
+void BulletManager::deleteBullet(std::string bulletId) {
+	for (auto it = bulletLists.begin(); it != bulletLists.end();) {
+		if ((*it)->id == bulletId) {
+			it = bulletLists.erase(it);
+		}
+		else {
+			it++;
+		}
+	}
+}
